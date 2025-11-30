5a3b2f63cbe6760651a404e42bb43b3477346bb5
AceHostWhistler <ben@acehost.ca>
2025-11-30 11:41:43 -0800

    Update Cedarhof property to link to contact page instead of Airbnb

--- src/pages/listings/cedarhof-kadenwood/index.tsx
+++ src/pages/listings/cedarhof-kadenwood/index.tsx
@@ -167,7 +167,7 @@ const CedarHof = () => {
             priceRange="$5,000-$6,500 per night Summer"
             winterPrice="$7,000-$9,000 Nightly | Winter"
             holidayPrice="$12,500-$16,500 Nightly | Christmas & NY"
-            airbnbLink="https://www.airbnb.ca/rooms/1457520373019699712?guests=1&adults=1&s=67&unique_share_id=112eabb2-8f04-425c-99c0-b02ddb2135e5"
+            contactLink="/contact"
           />
 
           {/* Photo Grid */}
--- src/pages/properties/index.tsx
+++ src/pages/properties/index.tsx
@@ -300,7 +300,7 @@ export default function Properties() {
             holidayPrice: "$12,500-$16,500 Nightly | Christmas & NY",
             isPetFriendly: false,
             isSkiInSkiOut: true,
-            airbnbLink: "https://www.airbnb.ca/rooms/1457520373019699712?guests=1&adults=1&s=67&unique_share_id=112eabb2-8f04-425c-99c0-b02ddb2135e5",
+            contactLink: "/contact",
           },
           {
             id: "altitude-retreat",
