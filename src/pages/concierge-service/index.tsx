cb871da23b3ec5446a1739e7634c60b4efe10178
AceHostWhistler <ben@acehost.ca>
2025-12-10 19:35:34 -0800

    Update ski pass image on concierge page

--- src/pages/concierge-service/index.tsx
+++ src/pages/concierge-service/index.tsx
@@ -212,7 +212,7 @@ const ConciergeService = () => {
                 <div className="bg-white rounded-lg overflow-hidden shadow-sm hover:shadow-md transition-shadow duration-300">
                   <div className="relative h-56">
                     <Image
-                      src="/texture/ski-pass-delivery.png"
+                      src="/thumbnails/Screenshot 2025-12-10 at 7.15.00 PM.png"
                       alt="Ski Lift Pass Delivery"
                       fill
                       className="object-cover"
